89160fc109a20ec4ca60ecf28942516fe0a5cf07
elliedavidson <118024407+elliedavidson@users.noreply.github.com>
2024-02-19 13:19:17 -0500

    Move over frame example from prev directory

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fun-with-frames
+
+This is a quick implementation of [Farcaster Frames](https://docs.farcaster.xyz/reference/frames/spec) deployed using Vercel. To customize, add the appropriate images in the `assets` directory.  Be sure to name them following the "slide-n.png" template. Next, update the `total_slides` variable in `app.py`. This example is meant to demonstrate registering for an event through a frame.  
+
+To run locally: `python -m flask run`
+
---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, send_file
+
+app = Flask(__name__)
+
+slide = 0
+total_slides = 3
+registration_url = "https://lu.ma/BTBL"
+registration_button_text = "Register"
+speakers_button_text = "Speakers"
+next_speaker_button_text = "Next Speaker"
+
+# Return one of the slide images, if it exists
+@app.route('/<path:subpath>', methods=['GET'])
+def image_handler(subpath):
+    try:
+        return send_file("./assets/" + subpath + ".png")
+    except: 
+        return "Not found"
+    
+
+# Return the correct template for a particlar slide
+@app.route('/', methods=['GET', 'POST'])
+def frame_handler():
+    global slide
+    global total_slides
+    global next_speaker_button_text
+    global registration_button_text
+    global speakers_button_text
+    global registration_url
+
+    if request.method == 'POST':
+        if slide < total_slides - 1:
+            slide += 1
+            slide_path = "slide-" + str(slide)
+            return render_template("slide-n.html", frame_image=(request.url + slide_path), og_image=(request.url + slide_path), left_button_text=next_speaker_button_text, left_button_target=request.url, right_button_text=registration_button_text, right_button_target=registration_url)
+        else: 
+            slide = 0
+            slide_path = "slide-" + str(slide)
+            return render_template("slide-n.html", frame_image=(request.url + slide_path), og_image=(request.url + slide_path), left_button_text=speakers_button_text, left_button_target=request.url, right_button_text=registration_button_text, right_button_target=registration_url)
+    else:
+        slide_path = "slide-" + str(slide)
+        return render_template("slide-n.html", frame_image=(request.url + slide_path), og_image=(request.url + slide_path), left_button_text=speakers_button_text, left_button_target=request.url, right_button_text=registration_button_text, right_button_target=registration_url)
+        
